4b53fe4681b81665a477c7817acd5133f1d5cf11
Wadim <42088646+wb-08@users.noreply.github.com>
2020-11-15 20:00:18 +0300

    Create part_2.py

---
+++ part_2.py
@@ -0,0 +1,56 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('girl.jpg')
+
+
+def resizing(new_width=None, new_height=None, interp=cv2.INTER_LINEAR):
+    h, w = img.shape[:2]
+
+    if new_width is None and new_height is None:
+        return img
+
+    if new_width is None:
+        ratio = new_height / h
+        dimension = (int(w * ratio), new_height)
+
+    else:
+        ratio = new_width / w
+        dimension = (new_width, int(h * ratio))
+
+    res_img = cv2.resize(img, dimension, interpolation=interp)
+    cv2.imwrite('new_girl_resize.jpg', res_img)
+
+    # res_img = cv2.resize(img, (500, 900), cv2.INTER_NEAREST)
+    # cv2.imwrite('res_girl.jpg', res_img)
+    res_img_nearest = cv2.resize(img, (int(w / 1.4), int(h / 1.4)), cv2.INTER_NEAREST)
+    res_img_linear = cv2.resize(img, (int(w / 1.4), int(h / 1.4)), cv2.INTER_LINEAR)
+    # cv2.imwrite('girl_nearest.jpg', res_img_nearest)
+    # cv2.imwrite('girl_linear.jpg', res_img_linear)
+
+
+def concatenate_images():
+    nearest_image = cv2.imread('girl_nearest.jpg')
+    linear_image = cv2.imread('girl_linear.jpg')
+    conc_image = np.concatenate(([nearest_image, linear_image]), axis=1)
+    cv2.imwrite('conc_girl.jpg', conc_image)
+
+
+def shifting():
+    h, w = img.shape[:2]
+    translation_matrix = np.float32([[1, 0, 200], [0, 1, 300]])
+    dst = cv2.warpAffine(img, translation_matrix, (w, h))
+    cv2.imwrite('girl_right_and_down_1.jpg', dst)
+
+
+def cropping():
+    crop_img = img[10:450, 300:750]
+    cv2.imwrite('crop_face.png', crop_img)
+
+
+def rotation():
+    (h, w) = img.shape[:2]
+    center = (int(w / 2), int(h / 2))
+    rotation_matrix = cv2.getRotationMatrix2D(center, 45, 0.6)
+    rotated = cv2.warpAffine(img, rotation_matrix, (w, h))
+    cv2.imwrite('rotated_girl.jpg', rotated)
