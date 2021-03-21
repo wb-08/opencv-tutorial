24cebacf53dfd748720f1444b993721dda144374
Wadim <42088646+wb-08@users.noreply.github.com>
2021-03-22 00:04:23 +0300

    Update part_1.py

--- part_1.py
+++ part_1.py
@@ -2,7 +2,7 @@ import cv2
 
 
 def loading_displaying_saving():
-    img = cv2.imread('girl.jpg', cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread('images/girl.jpg', cv2.IMREAD_GRAYSCALE)
     cv2.imshow('girl', img)
     cv2.waitKey(0)
     cv2.imwrite('graygirl.jpg', img)
@@ -10,7 +10,7 @@ def loading_displaying_saving():
 
 
 def accessing_and_manipulating():
-    img = cv2.imread('girl.jpg')
+    img = cv2.imread('images/girl.jpg')
     print("Высота:"+str(img.shape[0]))
     print("Ширина:" + str(img.shape[1]))
     print("Количество каналов:" + str(img.shape[2]))
